73af89f47e92672dbcddb83f8fbdd42b00d1c361
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2025-02-01 16:10:41 -0500

    a

--- solution/scraper/src/main.py
+++ solution/scraper/src/main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from celery.result import AsyncResult
 from tasks import extract_data
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List
+from database import SessionLocal, ScrapingResult
 
 app = FastAPI()
 
-class URLList(BaseModel):
-    urls: List[str]
+class FileName(BaseModel):
+    filename: str
+
+def read_links_from_file(filename: str) -> list[str]:
+    try:
+        with open(filename, "r") as file:
+            links = [line.strip() for line in file if line.strip()]
+        return links
+    except FileNotFoundError:
+        raise ValueError(f"El archivo '{filename}' no existe.")
+    except Exception as e:
+        raise ValueError(f"Error al leer el archivo: {e}")
+
 
 @app.get("/")
 def read_root():
@@ -17,19 +29,27 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/process")
-async def process_url(url_list: URLList):
-     task_ids = []
-    
-    # Enviar las tareas a Celery
-     for url in url_list.urls:
-        print(f"📩 Enviando tarea para {url}...")
-        task = extract_data.delay(url)
-        task_ids.append(task.id)
+async def process_file(file: FileName):
+    try:
         
-     return {"message": "Tareas enviadas", "task_ids": task_ids}
+        links = read_links_from_file(file.filename)
+        print(f"📄 Enlaces leídos: {links}")  # Borrar despues
+
+        task_ids = []
+                
+        for link in links:            
+            task = extract_data.delay(link)
+            task_ids.append(task.id)
+        
+        return {"message": "Tareas enviadas", "task_ids": task_ids}
     
-    # Enviar las tareas a Celery
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno: {e}")
+
 @app.get("/task/{task_id}")
 async def get_task_result(task_id: str):
     task_result = AsyncResult(task_id)
@@ -41,4 +61,10 @@ async def get_task_result(task_id: str):
         return {"status": "FAILED", "task_id": task_id, "error": str(task_result.result)}
     
     return {"status": "SUCCESS", "task_id": task_id, "result": task_result.result}
-    
+
+@app.get("/results")
+async def get_results():
+    db = SessionLocal()
+    results = db.query(ScrapingResult).all()
+    db.close()
+    return results
